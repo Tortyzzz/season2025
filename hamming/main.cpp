79b7d458920ebb72ae53faee99cf56456929bf44
evgeny emelyanov <evgen10.emelyanov@gmail.com>
2025-12-22 15:31:16 +0700

    Update main.cpp

--- hamming/main.cpp
+++ hamming/main.cpp
@@ -10,7 +10,7 @@ int main() {
     vector<vector<int>> testMessages0 = {
         { 0, 1, 0, 0, 1, 0, 0 }
     };
-    vector<vector<int>> testMessages4 = {
+    vector<vector<int>> testMessages1 = {
         { 1, 1, 0, 0, 1, 0, 1 }
     };
     vector<vector<int>> testMessages2 = {
@@ -18,6 +18,9 @@ int main() {
     };
     vector<vector<int>> testMessages3 = {
         { 1, 1, 0, 1, 1, 0, 0 }
+    };
+    vector<vector<int>>testMessages5 = {
+        { 1, 0, 0, 1, 1, 0, 0 }
     };
 
     // Тест 1: Одна ошибка
@@ -39,45 +42,26 @@ int main() {
         }
         cout << endl;
     }
-    for (size_t i = 0; i < testMessages4.size(); i++) {
+    for (size_t i = 0; i < testMessages1.size(); i++) {
         cout << "Тест 2 " << endl;
-        printVector(testMessages4[i], "Исходное сообщение");
+        printVector(testMessages1[i], "Исходное сообщение");
 
-        vector<int> hammingCode4 = generateHammingCode(testMessages4[i]);
-        printVector(hammingCode4, "Закодированное сообщение для проверки ");
+        vector<int> hammingCode1 = generateHammingCode(testMessages1[i]);
+        printVector(hammingCode1, "Закодированное сообщение для проверки ");
 
-        vector<int> corruptedCode4 = hammingCode4;
-        if (hammingCode4.size() > 2) {
-            corruptedCode4[4] ^= 1; // принудительная ошибка
-            printVector(corruptedCode4, "Принятый код  ");
+        vector<int> corruptedCode1 = hammingCode1;
+        if (hammingCode1.size() > 2) {
+            corruptedCode1[4] ^= 1; // принудительная ошибка
+            printVector(corruptedCode1, "Принятый код  ");
 
-            auto result = decodeHammingCode(corruptedCode4);
+            auto result = decodeHammingCode(corruptedCode1);
             cout << "Ошибка в позиции: " << result.second << endl;
             printVector(result.first, "Исправленное сообщение");
         }
         cout << endl;
     }
-    for (size_t i = 0; i < testMessages3.size(); i++) {
-        cout << "Тест 3" << endl;
-        printVector(testMessages3[i], "Исходное сообщение");
-
-        vector<int> hammingCode3 = generateHammingCode(testMessages3[i]);
-        printVector(hammingCode3, "Закодированное сообщение для проверки ");
-
-        vector<int> corruptedCode3 = hammingCode3;
-        if (hammingCode3.size() > 2) {
-            corruptedCode3[2] ^= 1; // принудительная ошибка
-            printVector(corruptedCode3, "Принятый код  ");
-
-            auto result = decodeHammingCode(corruptedCode3);
-            cout << "Ошибка в позиции: " << result.second << endl;
-            printVector(result.first, "Исправленное сообщение");
-        }
-        cout << endl;
-    }
-
     for (size_t i = 0; i < testMessages2.size(); i++) {
-        cout << "Тест 4 "<< endl;
+        cout << "Тест 3" << endl;
         printVector(testMessages2[i], "Исходное сообщение");
 
         vector<int> hammingCode2 = generateHammingCode(testMessages2[i]);
@@ -85,7 +69,7 @@ int main() {
 
         vector<int> corruptedCode2 = hammingCode2;
         if (hammingCode2.size() > 2) {
-            corruptedCode2[5] ^= 1; // принудительная ошибка
+            corruptedCode2[2] ^= 1; // принудительная ошибка
             printVector(corruptedCode2, "Принятый код  ");
 
             auto result = decodeHammingCode(corruptedCode2);
@@ -95,23 +79,58 @@ int main() {
         cout << endl;
     }
 
-    vector<int> testMessages1 = { 0, 1, 0, 0, 1, 0, 0 };
+    for (size_t i = 0; i < testMessages3.size(); i++) {
+        cout << "Тест 4 "<< endl;
+        printVector(testMessages3[i], "Исходное сообщение");
+
+        vector<int> hammingCode3 = generateHammingCode(testMessages3[i]);
+        printVector(hammingCode3, "Закодированное сообщение для проверки ");
+
+        vector<int> corruptedCode3 = hammingCode3;
+        if (hammingCode3.size() > 2) {
+            corruptedCode3[5] ^= 1; // принудительная ошибка
+            printVector(corruptedCode3, "Принятый код  ");
+
+            auto result = decodeHammingCode(corruptedCode3);
+            cout << "Ошибка в позиции: " << result.second << endl;
+            printVector(result.first, "Исправленное сообщение");
+        }
+        cout << endl;
+    }
+    for (size_t i = 0; i < testMessages5.size(); i++) {
+        cout << "Тест 5 " << endl;
+        printVector(testMessages5[i], "Исходное сообщение");
+
+        vector<int> hammingCode5 = generateHammingCode(testMessages5[i]);
+        printVector(hammingCode5, "Закодированное сообщение для проверки ");
+
+        vector<int> corruptedCode5 = hammingCode5;
+        if (hammingCode5.size() > 2) {
+            corruptedCode5[7] ^= 1; // принудительная ошибка
+            printVector(corruptedCode5, "Принятый код  ");
+
+            auto result = decodeHammingCode(corruptedCode5);
+            cout << "Ошибка в позиции: " << result.second << endl;
+            printVector(result.first, "Исправленное сообщение");
+        }
+        cout << endl;
+    }
+
+    vector<int> testMessages4 = { 0, 1, 0, 0, 1, 0, 0 };
     cout << "Тест с 2-мя ошибками" << endl;
-    printVector(testMessages1, "Исходное сообщение");
+    printVector(testMessages4, "Исходное сообщение");
 
-    vector<int> hammingCode = generateHammingCode(testMessages1);
-    printVector(hammingCode, "Закодированное сообщение для проверки ");
+    vector<int> hammingCode4 = generateHammingCode(testMessages4);
+    printVector(hammingCode4, "Закодированное сообщение для проверки ");
 
-    vector<int> corruptedCode1 = hammingCode;
-    if (hammingCode.size() > 2) {
-        corruptedCode1[3] ^= 1;
-        corruptedCode1[4] ^= 1; // принудительная ошибка
-        printVector(corruptedCode1, "Принятый код  ");
+    vector<int> corruptedCode4 = hammingCode4;
+    if (hammingCode4.size() > 2) {
+        corruptedCode4[3] ^= 1;
+        corruptedCode4[4] ^= 1; // принудительная ошибка
+        printVector(corruptedCode4, "Принятый код  ");
 
-        auto result = decodeHammingCode(corruptedCode1);
+        auto result = decodeHammingCode(corruptedCode4);
         cout << "Ошибка в позиции: " << result.second << endl;
         printVector(result.first, "Исправленное сообщение");
     }
-
-    return 0;
 }
